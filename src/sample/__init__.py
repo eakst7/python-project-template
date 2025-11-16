eakst7/git-rewrite

Initial commit - scaffolding

src/rewrite/__init__.py
@@ -1,0 +1,5 @@
+"""rewrite package."""
+
+__version__ = "0.1.0"
+
+from .main import main as main  # noqa: F401

src/rewrite/__main__.py
@@ -1,0 +1,9 @@
+"""Simple CLI for the rewrite package.
+
+Run with: python -m rewrite
+"""
+from .main import main
+
+if __name__ == "__main__":
+    main()
+

src/rewrite/main.py
@@ -1,0 +1,8 @@
+"""CLI entry point for the rewrite package."""
+from __future__ import annotations
+
+
+def main() -> None:
+    """Main CLI function."""
+    # TODO: Implement your CLI logic here
+    print("Hello from rewrite!")

src/rewrite/utils.py
@@ -1,0 +1,6 @@
+"""Utilities for the rewrite package."""
+
+from __future__ import annotations
+
+
+# TODO: Add utility functions here

tests/test_utils.py
@@ -1,0 +1,8 @@
+"""Tests for the rewrite package."""
+
+# TODO: Add your tests here
+
+
+def test_placeholder():
+    """Placeholder test."""
+    assert True

eakst7/python-project-template

src/sample/__init__.py
@@ -1,0 +1,5 @@
+"""sample package."""
+
+__version__ = "0.1.0"
+
+from .main import main as main  # noqa: F401

src/sample/__main__.py
@@ -1,0 +1,9 @@
+"""Simple CLI for the sample package.
+
+Run with: python -m sample
+"""
+from .main import main
+
+if __name__ == "__main__":
+    main()
+

src/sample/main.py
@@ -1,0 +1,8 @@
+"""CLI entry point for the sample package."""
+from __future__ import annotations
+
+
+def main() -> None:
+    """Main CLI function."""
+    # TODO: Implement your CLI logic here
+    print("Hello from sample!")

src/sample/utils.py
@@ -1,0 +1,6 @@
+"""Utilities for the sample package."""
+
+from __future__ import annotations
+
+
+# TODO: Add utility functions here

tests/test_utils.py
@@ -1,0 +1,8 @@
+"""Tests for the sample package."""
+
+# TODO: Add your tests here
+
+
+def test_placeholder():
+    """Placeholder test."""
+    assert True